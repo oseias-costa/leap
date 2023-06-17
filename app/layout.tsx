c5c7acb894a84610eae2218d9bf8618f27a9b142
Oséias Costa <oseiasc2@gmail.com>
2023-06-17 19:24:23 +0000

    create: description page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,46 @@
-import StyledComponentsRegistry from './lib/registry';
-import localFont from 'next/font/local'
-import { GlobalStyle } from './GlobalStyle';
+import StyledComponentsRegistry from "./lib/registry";
+import localFont from "next/font/local";
+import { GlobalStyle } from "./GlobalStyle";
 
 export const metadata = {
-  title: 'Leap Engenharia Química',
-  description: 'Leap Engenharia Química',
-}
+  title: "Leap Engenharia Química",
+  description:
+    "Impulsione seu negócio com a Leap. Regularizamos sua Empresa na ANVISA/MAPA, otimizamos seus processos industriais e trazemos sustentabilidade para sua operação.",
+};
 
 const Mollen = localFont({
-  display: 'swap',
+  display: "swap",
   src: [
     {
-      path: './font/Mollen-Light.otf',
-      weight: '300',
-      style: 'light'
+      path: "./font/Mollen-Light.otf",
+      weight: "300",
+      style: "light",
     },
     {
-      path: './font/Mollen-Regular.otf',
-      weight: '500',
-      style: 'normal'
+      path: "./font/Mollen-Regular.otf",
+      weight: "500",
+      style: "normal",
     },
     {
-      path: './font/Mollen-LightNarrow.otf',
-      weight: '200',
-      style: 'extraLight'
+      path: "./font/Mollen-LightNarrow.otf",
+      weight: "200",
+      style: "extraLight",
     },
   ],
-  variable: '--font-mollen'
+  variable: "--font-mollen",
 });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <GlobalStyle />
       <body className={`${Mollen.variable}`}>
-        <StyledComponentsRegistry>
-          {children}
-        </StyledComponentsRegistry>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
-  )
+  );
 }
